72a42428b110d8087e28eafb2090cd7ef9ae365a
Rob Di Marco <rob@elocal.com>
2019-01-08 08:39:09 -0500

    Refactor in to object

--- lib/ssm_to_env.rb
+++ lib/ssm_to_env.rb
@@ -3,14 +3,34 @@
 require "ssm_to_env/version"
 
 module SsmToEnv
+  class Loader
+    attr_reader :ssm_key_to_env_name_map, :target_hash, :with_decryption
+    def initialize(ssm_key_to_env_name_map, target_hash, with_decryption: :true)
+      @ssm_key_to_env_name_map = ssm_key_to_env_name_map
+      @target_hash = target_hash
+    end
+
+    def client
+      require 'aws-sdk-ssm'
+      @client ||= Aws::SSM::Client.new(region: region)
+    end
+
+    def ssm_parameters
+      @ssm_parameters ||= client.get_parameters(names: ssm_key_to_env_name_map.keys, with_decryption: true).parameters
+    end
+
+    def load!
+      ssm_parameters.each do |param|
+        target_hash[ssm_key_to_env_name_map[param.name]] = param.value
+      end
+
+      target_hash
+    end
+  end
   #
   # params - Hash with keys being AWS SSM keys and values being environment variables to set
   #
   def self.load!(ssm_key_to_env_name_map, region: 'us-east-1')
-    require 'aws-sdk-ssm'
-    client = Aws::SSM::Client.new(region: region)
-    client.get_parameters(names: ssm_key_to_env_name_map.keys, with_decryption: true).parameters.each do |param|
-      ENV[ssm_key_to_env_name_map[param.name]] = param.value
-    end
+    SsmToEnv::Loader.new(ssm_key_to_env_name_map, ENV).load!
   end
 end
